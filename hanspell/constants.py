6675b3a7747180fe2e153c253a52a541c6d4d023
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-10-28 02:17:14 +0900

    style: Black formatter 적용

--- hanspell/constants.py
+++ hanspell/constants.py
@@ -1,4 +1,4 @@
-base_url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy'
+base_url = "https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy"
 
 
 class CheckResult:
@@ -7,4 +7,3 @@ class CheckResult:
     WRONG_SPACING = 2
     AMBIGUOUS = 3
     STATISTICAL_CORRECTION = 4
-
--- setup.py
+++ setup.py
@@ -4,35 +4,35 @@ from setuptools import setup, find_packages
 
 def install():
     required = []
-    with open('requirements.txt') as f:
+    with open("requirements.txt") as f:
         requirements = f.read().splitlines()
         for req in requirements:
-            p = req.split('==')
+            p = req.split("==")
             required.append(p[0])
-    desc = ''
+    desc = ""
     setup(
-        name='py-hanspell',
-        version='1.1',
+        name="py-hanspell",
+        version="1.1",
         description=desc,
         long_description=desc,
-        author='SuHun Han',
-        author_email='ssut@ssut.me',
-        url='https://github.com/ssut/py-hanspell',
+        author="SuHun Han",
+        author_email="ssut@ssut.me",
+        url="https://github.com/ssut/py-hanspell",
         classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Education',
-            'Intended Audience :: End Users/Desktop',
-            'License :: Freeware',
-            'Operating System :: POSIX',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: MacOS :: MacOS X',
-            'Topic :: Education',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4'
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Education",
+            "Intended Audience :: End Users/Desktop",
+            "License :: Freeware",
+            "Operating System :: POSIX",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: MacOS :: MacOS X",
+            "Topic :: Education",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
         ],
         packages=find_packages(),
         install_requires=required,
